35cc512f9236f1952579bbba69a2db90aa7fa105
Kacper <qwe@wp.pl>
2023-07-06 11:34:26 +0200

    inventory view

--- MyGame/src/app/game-view/inventory/inventory.component.ts
+++ MyGame/src/app/game-view/inventory/inventory.component.ts
@@ -56,4 +56,51 @@ export class InventoryComponent {
       }
     },
   ]
+  inventory = [
+    {
+      name: 'Short Sword',
+      stats: {
+        strength: 5,
+        agility: 5,
+        intelligence: 0,
+      },
+      path: 'assets/items/short-sword.png'
+    },
+    {
+      name: 'Long Sword',
+      stats: {
+        strength: 10,
+        agility: 0,
+        intelligence: 0,
+      },
+      path: 'assets/items/long-sword.png'
+    },
+    {
+      name:'Gold Helmet',
+      stats: {
+        strength: 5,
+        agility: 5,
+        intelligence: 5,
+      },
+      path: 'assets/items/gold-helmet.png'
+    },
+    {
+      name: 'Light Helmet',
+      stats: {
+        strength: 0,
+        agility: 10,
+        intelligence: 0,
+      },
+      path: 'assets/items/light-helmet.png'
+    },
+    {
+      name: 'Heavy Helmet',
+      stats: {
+        strength: 10,
+        agility: 0,
+        intelligence: 0,
+      },
+      path: 'assets/items/heavy-helmet.png'
+    }
+]
 }
