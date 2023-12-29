183cbbf6e8c18db7792071f4e315bda8652c8f9a
Keenus <kacper.kinowski@interia.pl>
2023-12-29 15:24:40 +0100

    29.12.2023 update

--- MyGame/src/app/character-view/character-view.component.ts
+++ MyGame/src/app/character-view/character-view.component.ts
@@ -1,4 +1,5 @@
 import {Component, Output} from '@angular/core';
+import {CharactersService} from "../services/characters-service/characters.service";
 
 @Component({
   selector: 'app-character-view',
@@ -9,62 +10,19 @@ export class CharacterViewComponent {
 
   @Output() hero: any;
 
+  heroes: any[] = [];
 
-  heroes = [
-    { name: 'Knight',
-      img: 'woj.png',
-      selected: false,
-      details: 'Knight is a character that is very strong and has a lot of health points. He is a good choice for beginners.',
-      stats: {
-        strength: 7,
-        agility: 6,
-        intelligence: 5,
-        prosperity: 50,
-        health: 100,
-        level: 1,
-      }
-    },
-    {
-      name: 'Thief',
-      img: 'thief.png',
-      selected: false,
-      details: 'Thief is a character that is agile and inteligent. He is a fast learner and can be very strong in the future.',
-      stats: {
-        strength: 7,
-        agility: 15,
-        intelligence: 10,
-        prosperity: 60,
-        health: 100,
-        level: 3,
-      }
-    },
-    { name: 'Dwarf',
-      img: 'dwarf.png',
-      selected: false,
-      details: 'Dwarf is a character that is very strong but he is not very agile. His strength is his biggest advantage.',
-      stats: {
-        level: 1,
-        agility: 3,
-        strength: 10,
-        health: 100,
-        intelligence: 5,
-        prosperity: 50,
-      }
-    },
-    { name: 'Elf',
-      img: 'elf-woj.png',
-      selected: false,
-      details: 'Elf is a inteligent and agile character. He is not very strong but he is very fast and can be very strong in the future.',
-      stats: {
-        strength: 4,
-        agility: 8,
-        intelligence: 8,
-        prosperity: 75,
-        health: 100,
-        level: 1,
-      }
-    },
-  ];
+  constructor(
+    private charactersService: CharactersService
+  ) {
+    this.charactersService.getCharacters()
+      .subscribe(data => {
+        if(Array.isArray(data))
+          this.heroes = data
+        console.log(this.heroes)
+      })
+  }
+
 
   HeroChoose: any;
 
